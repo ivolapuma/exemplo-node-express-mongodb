f7d7bee4bb26b2b81a709a4aeaf959f4433fb4b7
Ivo La Puma <ivolapuma@gmail.com>
2021-04-18 20:17:44 -0300

    versao 2 com API POST recebendo dados usando express para parser do body

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 // configuracao do express
 const express = require('express');
-
 const app = express();
 
+// configuracao do body-parser
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+
+// para versoes do Express acima de 4.16, body-parser nao eh mais necessaio
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// configuracao da porta do servidor
 app.listen(3000, () => {
     console.log('Servidor rodando, escutando a porta 3000');
 });
 
-// criando 1a rota
+// rotas
 app.get('/', (request, response) => {
     response.send('Servidor rodando, tudo OK!');
+});
+
+app.post('/rastreador', (request, response) => {
+    console.log('Chamado método POST para cadastrar rastreamento');
+    console.log('request.body:');
+    console.log(request.body);
+    response.status(200).send('Chamado método POST para cadastrar rastreamento');
 });
