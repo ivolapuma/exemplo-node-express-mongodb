dc8e0c9776e24a6854a3413cb7ba0c328cab4085
Ivo La Puma <ivolapuma@gmail.com>
2021-04-19 18:54:50 -0300

    versao 10 com refatoracao da funcao cadastrar rastreamentos - retirada do async

--- controllers/rastreamento.js
+++ controllers/rastreamento.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
 
     const RastreamentoController = {
 
-        async cadastrar(request, response) {
+        cadastrar(request, response) {
 
             console.log('Chamado método POST para cadastrar rastreamento de um veiculo');
             console.log('request.body:');
@@ -18,47 +18,39 @@ module.exports = (app) => {
             const where = {
                 codigoRastreador: request.body.codigoRastreador
             };
-            const result = await Rastreador.find(
-                where,
-                (error, result) => {
-                    if (error) {
-                        const mensagem = `Erro ao localizar o codigoRastreador: ${error.message}`;
-                        console.log(mensagem);
-                        mongoose.disconnect();
-                        response.status(500).send(mensagem);
-                    } else if (result.length === 0) {
+
+            Rastreador.find(where)
+                .then((result) => {
+                    if (result.length === 0) {
                         const mensagem = `codigoRastreador ${where.codigoRastreador} não localizado`;
                         console.log(mensagem);
                         mongoose.disconnect();
                         response.status(404).send(mensagem);
                     } else {
-                        return result;
+                        const rastreamento = new Rastreamento(request.body);
+                        if (!rastreamento.dataHora) {
+                            rastreamento.dataHora = new Date;
+                        }
+                        rastreamento.save()
+                            .then((result) => {
+                                console.log(`Rastreamento do veiculo de codigoRastreador ${rastreamento.codigoRastreador} cadastrado com sucesso`);
+                                mongoose.disconnect();
+                                response.status(200).send(result);
+                            })
+                            .catch((error) => {
+                                const mensagem = `Erro ao cadastrar rastreamento: ${error.message}`;
+                                console.log(mensagem);
+                                mongoose.disconnect();
+                                response.status(500).send(mensagem);
+                            });
                     }
-                }
-            );
-
-            const rastreador = new Rastreador(result[0]);
-            console.log('codigoRastreador localizado com sucesso')
-            console.log(rastreador);
-            
-            const rastreamento = new Rastreamento(request.body);
-            if (!rastreamento.dataHora) {
-                rastreamento.dataHora = new Date;
-            }
-            rastreamento.save(
-                (error, result) => {
-                    if (error) {
-                        const mensagem = `Erro ao cadastrar rastreamento: ${error.message}`;
-                        console.log(mensagem);
-                        mongoose.disconnect();
-                        response.status(500).send(mensagem);
-                    } else {
-                        console.log(`Rastreamento do veiculo de codigoRastreador ${rastreamento.codigoRastreador} cadastrado com sucesso`);
-                        mongoose.disconnect();
-                        response.status(200).send(result);
-                    }
-                }
-            );            
+                })
+                .catch((error) => {
+                    const mensagem = `Erro ao localizar o codigoRastreador: ${error.message}`;
+                    console.log(mensagem);
+                    mongoose.disconnect();
+                    response.status(500).send(mensagem);
+                });
         },
 
         buscarPorRastreador(request, response) {
