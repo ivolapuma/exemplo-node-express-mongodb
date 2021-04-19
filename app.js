044a6dc8b8df643982de5f706bbe351762506584
Ivo La Puma <ivolapuma@gmail.com>
2021-04-18 21:54:31 -0300

    versao 4 com funcao cadastrar rastreador ok - metodo post rastreador

--- app.js
+++ app.js
@@ -7,11 +7,28 @@ const app = express();
 // app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(bodyParser.json())
 
+
 // para versoes do Express acima de 4.16, body-parser nao eh mais necessaio
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+
+// configuracao do mongoose
+const mongoose = require('mongoose');
+// definindo modelo do banco MongoDB
+// via aplicacao, vamos criar a coleção no mongodb...
+mongoose.connect('mongodb://localhost:27017/carlog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+const Schema = mongoose.Schema;
+const rastreadorSchema = Schema({
+    codigoRastreador: { type: String, required: true, index: { unique: true } },
+    placaVeiculo: { type: String, required: true },
+    cpfCliente: { type: String, required: true }
+});
+const Rastreador = mongoose.model('rastreadores', rastreadorSchema);
+mongoose.disconnect();
+
 
 // configuracao da porta do servidor
 app.listen(3000, () => {
@@ -24,8 +41,26 @@ app.get('/', (request, response) => {
 });
 
 app.post('/rastreador', (request, response) => {
+
     console.log('Chamado método POST para cadastrar rastreamento');
     console.log('request.body:');
     console.log(request.body);
-    response.status(200).send('Chamado método POST para cadastrar rastreamento');
+
+    // cadastro do rastreador
+    mongoose.connect('mongodb://localhost:27017/carlog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+    const rastreador = new Rastreador(request.body);
+    rastreador.save(
+        (error, result) => {
+            if (error) {
+                console.log(`Erro ao cadastrar o rastreador: ${error.message}`);
+                mongoose.disconnect();
+                response.status(500).send(`Erro ao cadastrar o rastreador: ${error.message}`);
+            } else {
+                console.log(`Rastreador ${rastreador.codigoRastreador} cadastrado no banco`);
+                mongoose.disconnect();
+                response.status(200).send(result);
+            }
+        }
+    );
+
 });
